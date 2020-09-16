b757cf4142c0ee8859b4b931146e0a75f39124f9
shanigu <shanigu@bgu.ac.il>
2020-09-16 08:59:17 +0300

    Add files via upload

---
+++ CropPlants/CropPlants/Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace CropPlants
+{
+    partial class frmCropPlants
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtTargetDir = new System.Windows.Forms.TextBox();
+            this.lblFiles = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lstFiles = new System.Windows.Forms.ListBox();
+            this.lstRectangles = new System.Windows.Forms.ListBox();
+            this.btnDelete = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(8, 90);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(903, 470);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(95, 18);
+            this.txtPath.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(717, 20);
+            this.txtPath.TabIndex = 1;
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Location = new System.Drawing.Point(28, 19);
+            this.lbl1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(63, 13);
+            this.lbl1.TabIndex = 2;
+            this.lbl1.Text = "Image path:";
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(943, 533);
+            this.btnNext.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(72, 27);
+            this.btnNext.TabIndex = 3;
+            this.btnNext.Text = "Next >";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(826, 12);
+            this.btnLoad.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(66, 29);
+            this.btnLoad.TabIndex = 6;
+            this.btnLoad.Text = "Load";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 52);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Target dir:";
+            // 
+            // txtTargetDir
+            // 
+            this.txtTargetDir.Location = new System.Drawing.Point(95, 50);
+            this.txtTargetDir.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtTargetDir.Name = "txtTargetDir";
+            this.txtTargetDir.Size = new System.Drawing.Size(717, 20);
+            this.txtTargetDir.TabIndex = 7;
+            // 
+            // lblFiles
+            // 
+            this.lblFiles.AutoSize = true;
+            this.lblFiles.Location = new System.Drawing.Point(981, 50);
+            this.lblFiles.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblFiles.Name = "lblFiles";
+            this.lblFiles.Size = new System.Drawing.Size(31, 13);
+            this.lblFiles.TabIndex = 9;
+            this.lblFiles.Text = "Files:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(1145, 52);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Rectangles:";
+            // 
+            // lstFiles
+            // 
+            this.lstFiles.FormattingEnabled = true;
+            this.lstFiles.Location = new System.Drawing.Point(943, 84);
+            this.lstFiles.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.lstFiles.Name = "lstFiles";
+            this.lstFiles.Size = new System.Drawing.Size(157, 446);
+            this.lstFiles.TabIndex = 11;
+            this.lstFiles.SelectedIndexChanged += new System.EventHandler(this.lstFiles_SelectedIndexChanged);
+            // 
+            // lstRectangles
+            // 
+            this.lstRectangles.FormattingEnabled = true;
+            this.lstRectangles.Location = new System.Drawing.Point(1121, 85);
+            this.lstRectangles.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.lstRectangles.Name = "lstRectangles";
+            this.lstRectangles.Size = new System.Drawing.Size(131, 446);
+            this.lstRectangles.TabIndex = 12;
+            this.lstRectangles.SelectedIndexChanged += new System.EventHandler(this.lstRectangles_SelectedIndexChanged);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(1129, 533);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(79, 26);
+            this.btnDelete.TabIndex = 13;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // frmCropPlants
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1260, 567);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.lstRectangles);
+            this.Controls.Add(this.lstFiles);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblFiles);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtTargetDir);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.lbl1);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "frmCropPlants";
+            this.Text = "Crop plants";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtTargetDir;
+        private System.Windows.Forms.Label lblFiles;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox lstFiles;
+        private System.Windows.Forms.ListBox lstRectangles;
+        private System.Windows.Forms.Button btnDelete;
+    }
+}
+
---
+++ CropPlants/CropPlants/Form1.cs
@@ -0,0 +1,292 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CropPlants
+{
+    public partial class frmCropPlants : Form
+    {
+
+        List<Rectangle> Rectangles;
+        Rectangle CurrentRectangle;
+        Point StartPoint, CurrentPoint;
+        Bitmap bmp;
+        bool MouseDown;
+        List<FileInfo> Files;
+        int CurrentFile;
+        Dictionary<FileInfo, List<FileInfo>> CroppedFiles;
+        private double XRatio, YRatio;
+
+        public frmCropPlants()
+        {
+            InitializeComponent();
+            MouseDown = false;
+            CurrentRectangle = new Rectangle(-1,-1,0,0);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            txtPath.Text = @"C:\Users\shanigu\Downloads\images";
+            txtTargetDir.Text = @"C:\Users\shanigu\Downloads\images\Cropped";
+            Rectangles = new List<Rectangle>();
+            CroppedFiles = new Dictionary<FileInfo, List<FileInfo>>();
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            for (int i = 0; i < Rectangles.Count; i++)
+            {
+                if (i == lstRectangles.SelectedIndex)
+                    g.DrawRectangle(Pens.Orange, Rectangles[i]);
+                else
+                    g.DrawRectangle(Pens.Red, Rectangles[i]);
+            }
+            if (CurrentRectangle.X > 0)
+                g.DrawRectangle(Pens.Red, CurrentRectangle);
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            MouseDown = true;
+            CurrentRectangle = new Rectangle(e.X, e.Y, 50, 50);
+            StartPoint = new Point(e.X, e.Y);
+            CurrentPoint = new Point(e.X + 50, e.Y + 50);
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            MouseDown = false;
+            Rectangles.Add(CurrentRectangle);
+            CurrentRectangle = new Rectangle(-1, -1, 0, 0);
+            WriteRectangles();
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (MouseDown == true)
+            {
+                
+                if (e.Y < pictureBox1.Height && e.Y > 0)
+                    CurrentPoint.Y = e.Y;
+                if (e.X < pictureBox1.Width && e.X > 0)
+                    CurrentPoint.X = e.X;
+
+                if (StartPoint.X < CurrentPoint.X)
+                {
+                    CurrentRectangle.X = StartPoint.X;
+                    CurrentRectangle.Width = CurrentPoint.X - StartPoint.X;
+                }
+                else
+                {
+                    CurrentRectangle.X = CurrentPoint.X;
+                    CurrentRectangle.Width = StartPoint.X - CurrentPoint.X;
+                }
+                if (StartPoint.Y < CurrentPoint.Y)
+                {
+                    CurrentRectangle.Y = StartPoint.Y;
+                    CurrentRectangle.Height = CurrentPoint.Y - StartPoint.Y;
+                }
+                else
+                {
+                    CurrentRectangle.Y = CurrentPoint.Y;
+                    CurrentRectangle.Height = StartPoint.Y - CurrentPoint.Y;
+                }
+
+
+                Refresh();
+            }
+        }
+
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            if (keyData == Keys.Delete)
+                OnKeyPress(new KeyPressEventArgs((Char)Keys.Delete));
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+
+        public void WriteRectangles()
+        {
+            lstRectangles.Items.Clear();
+            foreach (Rectangle r in Rectangles)
+                lstRectangles.Items.Add("[" + r.X + "," + r.Y + "," + (r.X + r.Width) + "," + (r.Y + r.Height) + "]");
+            lstRectangles.SelectedIndex = Rectangles.Count - 1;
+        }
+
+        private void DeleteSelectedRectangle()
+        {
+            if (Rectangles.Count > 0 && lstRectangles.SelectedIndex >= 0)
+            {
+                Rectangles.RemoveAt(lstRectangles.SelectedIndex);
+                WriteRectangles();
+                Refresh();
+            }
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (Char)Keys.Delete)
+            {
+                DeleteSelectedRectangle();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MinimizeBox = false;
+            MaximizeBox = false;
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            InNext = true;
+            try
+            {
+                DirectoryInfo dir = new DirectoryInfo(txtPath.Text);
+                lstFiles.ClearSelected();
+                lstFiles.Items.Clear();
+                Files = new List<FileInfo>();
+                PopulateList(dir);
+                lblFiles.Text = "Files (" + Files.Count + "):";
+                foreach (FileInfo fi in Files)
+                    lstFiles.Items.Add(fi.Name);
+                CurrentFile = 0;
+                if(Files.Count > 0)
+                {
+                    LoadImage();
+                    lstFiles.SelectedIndex = CurrentFile;
+
+                    Rectangles = new List<Rectangle>();
+                }
+            }
+            catch(Exception ex)
+            {
+
+            }
+            InNext = false;
+        }
+
+        
+
+        private void PopulateList(DirectoryInfo dir)
+        {
+            foreach(FileInfo file in dir.GetFiles())
+            {
+                if(file.Extension.ToLower().Contains("png" ))
+                {
+                    if (file.Name.ToLower().Contains("_color_"))
+                        Files.Add(file);
+                }
+            }
+            foreach (DirectoryInfo dSub in dir.GetDirectories())
+                PopulateList(dSub);
+        }
+
+
+        public  Bitmap CropAtRect(Bitmap b, Rectangle r)
+        {
+            Bitmap nb = new Bitmap((int)(r.Width * XRatio), (int)(r.Height * YRatio));
+            using (Graphics g = Graphics.FromImage(nb))
+            {
+                g.DrawImage(b, (int)-(r.X * XRatio), (int)-(r.Y * YRatio));
+                return nb;
+            }
+        }
+
+        bool InNext = false;
+
+        private void SaveRectangles()
+        {
+            try
+            {
+                string sTargetDir = txtTargetDir.Text;
+                string sJPEGDir = sTargetDir + "/JPEGS";
+                if (!Directory.Exists(sTargetDir))
+                    Directory.CreateDirectory(sTargetDir);
+                if (!Directory.Exists(sJPEGDir))
+                    Directory.CreateDirectory(sJPEGDir);
+
+                StreamWriter sw = new StreamWriter(sTargetDir + "/Rectangles.txt", true);
+                string sLine = Files[CurrentFile].FullName + "\t" + Rectangles.Count;
+                foreach (Rectangle r in Rectangles)
+                    sLine += "\t [" + r.X + "," + r.Y + "," + (r.X + r.Width) + "," + (r.Y + r.Height) + "]";
+                sw.WriteLine(sLine);
+                sw.Close();
+
+                foreach (Rectangle r in Rectangles)
+                {
+                    Bitmap bmpCropped = CropAtRect(bmp, r);
+                    string sFileName = Files[CurrentFile].Name.Replace(".png", "") + "_" + r.X + "_" + r.Y + "_" + (r.X + r.Width) + "_" + (r.Y + r.Height);
+                    bmpCropped.Save(sTargetDir + "\\" + sFileName + ".png");
+                    bmpCropped.Save(sJPEGDir + "\\" + sFileName + ".jpeg", ImageFormat.Jpeg);
+                }
+
+                lstFiles.Items[CurrentFile] = "v " + lstFiles.Items[CurrentFile];
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            InNext = true;
+            try
+            {
+                SaveRectangles();
+
+                CurrentFile++;
+                if (CurrentFile < Files.Count)
+                {
+                    lstFiles.SelectedIndex = CurrentFile;
+                    LoadImage();
+                    Rectangles = new List<Rectangle>();
+                    lstRectangles.Items.Clear();
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+            InNext = false;
+        }
+
+        private void lstFiles_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(!InNext)
+            {
+                SaveRectangles();
+                CurrentFile = lstFiles.SelectedIndex;
+                LoadImage();
+                Rectangles = new List<Rectangle>();
+                lstRectangles.Items.Clear();
+            }
+        }
+
+        private void lstRectangles_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Refresh();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            DeleteSelectedRectangle();
+        }
+
+        private void LoadImage()
+        {
+            bmp = new Bitmap(Files[CurrentFile].FullName);
+            pictureBox1.Image = bmp;
+            XRatio = (1.0 * bmp.Width) / pictureBox1.Width;
+            YRatio = (1.0 * bmp.Height) / pictureBox1.Height;
+        }
+    }
+}
